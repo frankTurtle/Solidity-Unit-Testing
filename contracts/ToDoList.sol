72603245d68d522a44d66cdc279087346408053b
Big B <8c57e820-f3a7-4561-a664-fed7d85bdedc@4wrd.cc>
2021-12-12 14:34:24 -0500

    npm init and basic contract for ToDo list

---
+++ contracts/ToDoList.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.0;
+ 
+contract TodoList {
+    uint256 public taskCount = 0;
+ 
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+ 
+    mapping(uint256 => Task) public tasks;
+ 
+    function createTask(string memory _content) public {
+        taskCount++;
+ 
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+}
